a296842aeae5c57f09112d31b759f6f7086e1c02
Mike Anderson <45513948+MikeAndrson@users.noreply.github.com>
2024-04-23 23:42:35 +0300

    Sitemap and robots.txt

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import devServer from "@hono/vite-dev-server";
 import { defineConfig } from "vite";
 import ssg from "@hono/vite-ssg";
+import Sitemap from "vite-plugin-sitemap";
 
 export default defineConfig({
   plugins: [
@@ -8,6 +9,10 @@ export default defineConfig({
       entry: "src/index.tsx",
     }),
     ssg(),
+    Sitemap({
+      hostname: "https://omeraydin.dev/",
+      exclude: ["/404"],
+    }),
   ],
   assetsInclude: "**/*.md",
 });
